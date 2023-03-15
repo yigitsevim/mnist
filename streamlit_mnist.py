20440e10eda49983f18dc49793c3976b9c265be6
Yigit Sevim <58977041+yigitsevim@users.noreply.github.com>
2023-03-14 22:36:49 -0400

    Update streamlit_mnist.py

--- streamlit_mnist.py
+++ streamlit_mnist.py
@@ -72,8 +72,9 @@ def predict(model, categories, image):
 
 def main():
     st.title('Pretrained model demo')
-    device = torch.device('cpu')
+    device = (torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')))
     model = load_model()
+    model = model.to(device)
     categories = [0,1,2,3,4,5,6,7,8,9]
     image = load_image()
     result = st.button('Run on image')

83b60bd77767c27d5033affb3a347592259fe143
Yigit Sevim <58977041+yigitsevim@users.noreply.github.com>
2023-03-14 22:34:04 -0400

    Add files via upload

--- streamlit_mnist.py
+++ streamlit_mnist.py
@@ -7,27 +7,25 @@ from torchvision import transforms
 import wget
 import torch.nn as nn
 
-device = (torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'))
-
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        
+
         self.linear1 = nn.Linear(28**2, 100, bias=False)
         self.bn1 = nn.BatchNorm1d(100)
         self.linear2 = nn.Linear(100, 50, bias=False)
         self.bn2 = nn.BatchNorm1d(50)
         self.linear3 = nn.Linear(50, 10, bias=False)
         self.R = nn.ReLU()
-        
+
         self.history = []
         self.batch_history = []
-        self.val_history = []  
-        
+        self.val_history = []
+
     def forward(self, x):
         self.linear1ins, self.linear1outs = [], []
         self.linear2outs = []
-        
+
         x = x.to(dtype=torch.float32)
         x = x.to(device)
         x = x.view(-1, 28**2)
@@ -39,7 +37,7 @@ class Net(nn.Module):
         x = self.R(self.bn2(x))
         self.linear2outs.append(x)
         x = self.linear3(x)
-        
+
         return x
 
 def load_image():
@@ -62,7 +60,7 @@ def predict(model, categories, image):
     ])
     input_tensor = preprocess(image)
     input_batch = input_tensor.unsqueeze(0)
- 
+
     with torch.no_grad():
         output = model(input_batch)
 
@@ -72,18 +70,18 @@ def predict(model, categories, image):
     for i in range(top5_prob.size(0)):
         st.write(categories[top5_catid[i]], top5_prob[i].item())
 
-    def main():
-        st.title('Pretrained model demo')
-        device = device
-        model = load_model()
-        categories = [0,1,2,3,4,5,6,7,8,9]
-        image = load_image()
-        result = st.button('Run on image')
-        if result:
-            st.write('Calculating results...')
-            predict(model, categories, image)
+def main():
+    st.title('Pretrained model demo')
+    device = (torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'))
+    model = load_model()
+    categories = [0,1,2,3,4,5,6,7,8,9]
+    image = load_image()
+    result = st.button('Run on image')
+    if result:
+        st.write('Calculating results...')
+        predict(model, categories, image)
 
-    if __name__ == '__main__':
-        print('main')
-        main()
-    else:print('degil')
+if __name__ == '__main__':
+    main()
+else:
+    print('degil')
